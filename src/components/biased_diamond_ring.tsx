3b76fa321d657613825a4d5e9fd6898ef92db043
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-14 21:15:44 +0100

    Update functions

--- src/components/biased_diamond_ring.tsx
+++ src/components/biased_diamond_ring.tsx
@@ -54,28 +54,29 @@ const BiasedDiamondRing: React.FunctionComponent = () => {
         return 0
       })
 
+      // console.log(points)
+
       // Show points length
-      p5Obj
-        .fill(51)
-        .textAlign(p5Obj.RIGHT, p5Obj.BOTTOM)
-        .textSize(10)
-        .text(points.length, p5Obj.width - 10, p5Obj.height - 10)
+      p5Obj.fill(51)
+      p5Obj.textAlign(p5Obj.RIGHT, p5Obj.BOTTOM)
+      p5Obj.textSize(10)
+      p5Obj.text(points.length, p5Obj.width - 10, p5Obj.height - 10)
     }
   }
 
   // See annotations in JS for more information
-  const setup = (p5: p5Types, canvasParentRef: Element) => {
+  const setup = (p5: p5Types, parentRef: Element) => {
     setP5Obj(p5)
-    p5.createCanvas(window.innerWidth, window.innerHeight).parent(
-      canvasParentRef
-    )
+    p5.createCanvas(window.innerWidth, window.innerHeight).parent(parentRef)
 
     cx = p5.width / 2
     cy = p5.height / 2
     r = p5.min(p5.width, p5.height) / 2 - 80
 
-    p5.background(255).stroke(0).strokeWeight(0.1).noFill()
-
+    p5.background(255)
+    p5.stroke(0)
+    p5.strokeWeight(0.1)
+    p5.noFill()
     setPoints()
   }
 
@@ -84,8 +85,8 @@ const BiasedDiamondRing: React.FunctionComponent = () => {
       // Draw outer circle
       if (cnt === 0) {
         p5.stroke(127)
-          .circle(cx, cy, r * 2)
-          .stroke(0)
+        p5.circle(cx, cy, r * 2)
+        p5.stroke(0)
       }
 
       // Draw the diamond ring
@@ -96,6 +97,19 @@ const BiasedDiamondRing: React.FunctionComponent = () => {
       cnt++
       if (cnt >= points.length) {
         setWaitFlag(true)
+      }
+    } else {
+      // Wait on complete
+      waitTime++
+      if (waitTime > 300) {
+        cnt = 0
+        p5.clear()
+        p5.background(255)
+        p5.noFill()
+        setPoints()
+
+        waitTime = 0
+        setWaitFlag(false)
       }
     }
   }
